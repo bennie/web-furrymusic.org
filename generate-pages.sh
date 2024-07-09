7f217df2e7a8c67a69e7b120d208c304cb7b3e8a
Phillip Pollard <phil@crescendo.net>
2024-07-09 17:30:41 -0400

    Starting a new site.

---
+++ generate-pages.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Remove the local build
+if [ -d www.furrymusic.org ]; then rm -rfv www.furrymusic.org; fi
+
+# Copy static data into the site layout
+rsync -av --exclude='.git/' --exclude='.DS_Store' static/ www.furrymusic.org;
+
+# Update Jekyll and include it's output
+cd jekyll; bundle exec jekyll build; cd ..;
+
+# Copy the rendered Jekyll output into the site layout
+rsync -av --exclude='.git/' --exclude='.DS_Store' jekyll/_site/ www.furrymusic.org
+
+# Publish the combined site layout on to the server
+rsync -av --exclude='.git/' --exclude='.DS_Store' --delete www.furrymusic.org/ ${MY_WEBUSER}@${MY_WEBHOST}:/var/www/furrymusic.org/www
