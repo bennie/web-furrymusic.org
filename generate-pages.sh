057d92d88e43fbf1db68dd14156d7c29b0e04c9e
Phillip Pollard <phil@crescendo.net>
2024-07-11 14:53:24 -0400

    Initial play with the new template.

--- generate-pages.sh
+++ generate-pages.sh
@@ -9,10 +9,10 @@ if [ -d www.furrymusic.org ]; then rm -rfv www.furrymusic.org; fi
 rsync -av --exclude='.git/' --exclude='.DS_Store' static/ www.furrymusic.org;
 
 # Update Jekyll and include it's output
-cd jekyll; bundle exec jekyll build; cd ..;
+cd feeling-responsive; bundle exec jekyll build; cd ..;
 
 # Copy the rendered Jekyll output into the site layout
-rsync -av --exclude='.git/' --exclude='.DS_Store' jekyll/_site/ www.furrymusic.org
+rsync -av --exclude='.git/' --exclude='.DS_Store' feeling-responsive/_site/ www.furrymusic.org
 
 # Publish the combined site layout on to the server
 rsync -av --exclude='.git/' --exclude='.DS_Store' --delete www.furrymusic.org/ ${MY_WEBUSER}@${MY_WEBHOST}:/var/www/furrymusic.org/www
